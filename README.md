546f77bd7bacba6654f3a97f3566d976a59ecdf6
The Meta <alexmeta517@gmail.com>
2023-02-28 07:19:17 +0300

    Hash Fuction

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 **HASH TABLE** &rarr; a type of data structure in which the address or the index value of the data element is generated from a hash function. That makes accessing the data faster as the index value behaves as a key for the data value.
 
+**HASH FUCTION** &rarr; Used to convert strings and non-numeric data types into numbers, which can then be used as list indices.
+
 ![Hash Table](https://svbtleusercontent.com/dvp2mqhvzcqyqa.jpg)
---
+++ hashtable.py
@@ -0,0 +1,26 @@
+# Create a list that will store all the key value pairs
+# Give it a constant length 
+
+LIST_MAX_SIZE = 4096
+
+key_value_list = [None] * LIST_MAX_SIZE
+
+# Lets create a hash class
+
+class HashTable:
+
+    #Insert fuction
+    def insert(self, key, value):
+        pass
+
+    #Find fuction
+    def find(self, key):
+        pass
+
+    #Update fuction
+    def update(self, key, value):
+        pass
+
+    #List all items
+    def listAll(self):
+        pass
